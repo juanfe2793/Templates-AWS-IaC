9b3fc2ebc5b6a754890a2b8b9fbb53c3a7c67a27
Juan Felipe Gómez <juanfe.2793@gmail.com>
2019-10-23 23:36:08 -0500

    Demo App Template Full
    Se corrijen los errores del User Data para descargar la aplicación web e implementarla.

--- WebServer-SimplePHPapp.sh
+++ WebServer-SimplePHPapp.sh
@@ -8,9 +8,9 @@ amazon-linux-extras install -y php7.2
 
 #Download web app
 
-#wget https://us-west-2-tcprod.s3.amazonaws.com/courses/AWS-100-CCA/v3.1.21/lab8-ha/scripts/phpapp.zip
+wget https://us-west-2-tcprod.s3.amazonaws.com/courses/AWS-100-CCA/v3.1.21/lab8-ha/scripts/phpapp.zip
 unzip phpapp.zip -d /var/www/html/
-service httpd start
+
 
 # Turn on web server
 chkconfig httpd on
