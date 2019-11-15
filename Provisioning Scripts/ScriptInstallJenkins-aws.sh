21be77b57ca4853db1768921c079df6af8959a32
Juan Felipe Gómez <juanfe.2793@gmail.com>
2019-11-15 02:07:44 -0500

    Corrección Scripts de aprovisionamiento

--- Provisioning Scripts/ScriptInstallJenkins-aws.sh
+++ Provisioning Scripts/ScriptInstallJenkins-aws.sh
@@ -1,18 +1,20 @@
-#!/bin/bash -xe
-# Description: Este es un script para automatizar el proceso de instalación del servidor de integración continua con jenkins. 
-# El script se pasa como UserData en una instancia EC2 de AWS. 
+#!/bin/bash
+# Description: Este es un script para automatizar el proceso de instalacion del servidor de integracion continua con jenkins. 
+# El script se pasa como UserData en una instancia EC2 de AWS con S.O. RedHat. 
 yum update -y
-#Instalación java y nodejs.
-yum install -y java-1.8.0-openjdk
-#Instalación de NodeJS con nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+#Instalacion java y nodejs.
+yum install -y java-1.8.0-openjdk 
+yum install -y nodejs wget git
 npm install -g forever
-#Descarga del repositorio de Jenkins y su instalación.
+#Descarga del repositorio de Jenkins y su instalacion.
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm -import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 yum install -y jenkins
+#Configurar permisos para conexion con repositorio CodeCommit. Es necesario para que AWS acepte las claves HTTPs del usuario.
+sudo -u jenkins git config --global credential.helper '!aws codecommit credential-helper $@'
+sudo -u jenkins git config --global credential.useHttpPath true
+sudo -u jenkins git config --global user.email "me@mycompany.com"
+sudo -u jenkins git config --global user.name "MyJenkinsServer"
 #Configurar Jenkins para que arranque en el inicio de la instancia. 
 chkconfig jenkins on
 #Iniciar Servicio Jenkins
