035aad4cfee9d440f1b058eb3c199e9518f5306f
Juan Felipe Gomez <juanfe.2793@gmail.com>
2020-01-08 09:16:02 -0500

    Actualización Script-Renombrar.

--- Provisioning Scripts/DemoApp-Location-EC2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-#Descripcion: 
-    yum update -y 
-    yum install -y httpd php aws-cfn-bootstrap
-    amazon-linux-extras install -y php7.2
-    wget https://us-west-2-tcprod.s3.amazonaws.com/courses/AWS-100-CCA/v3.1.21/lab8-ha/scripts/phpapp.zip
-    unzip phpapp.zip -d /var/www/html/
-    chkconfig httpd on
-    service httpd start
